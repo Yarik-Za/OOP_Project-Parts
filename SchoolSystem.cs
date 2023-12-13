1b73d6f4d6cc2db8d4b5275f7dd4cdab67839b79
Yarik Zaychenko <yarikyarik20017@gmail.com>
2023-12-13 23:50:28 +0200

    каркас проєкту

--- SchoolSystem.cs
+++ SchoolSystem.cs
@@ -2,52 +2,57 @@
 
 namespace Project_part_A
 {
-    public class SchoolSystem
+    // SchoolSystem.cs
+    using System;
+    using System.Collections.Generic;
+
+    namespace Project_part_A
     {
-        private List<SchoolClass> Classes { get; }
-        private List<Teacher> Teachers { get; }
-        private List<Student> Students { get; }
-
-        public SchoolSystem(List<SchoolClass> classes, List<Teacher> teachers, List<Student> students)
+        public class SchoolSystem
         {
-            Classes = classes;
-            Teachers = teachers;
-            Students = students;
-        }
+            private List<SchoolClass> Classes { get; }
+            private List<Teacher> Teachers { get; }
+            private List<Student> Students { get; }
 
-        public void AddClass(SchoolClass schoolClass)
-        {
-            Classes.Add(schoolClass);
-        }
+            public SchoolSystem(List<SchoolClass> classes, List<Teacher> teachers, List<Student> students)
+            {
+                throw new NotImplementedException();
+            }
 
-        public List<SchoolClass> GetClasses()
-        {
-            return Classes;
-        }
+            public void AddClass(SchoolClass schoolClass)
+            {
+                throw new NotImplementedException();
+            }
 
-        public void AddTeacher(Teacher teacher)
-        {
-            Teachers.Add(teacher);
-        }
+            public List<SchoolClass> GetClasses()
+            {
+                throw new NotImplementedException();
+            }
 
-        public List<Teacher> GetTeachers()
-        {
-            return Teachers;
-        }
+            public void AddTeacher(Teacher teacher)
+            {
+                throw new NotImplementedException();
+            }
 
-        public void AddStudent(Student student)
-        {
-            Students.Add(student);
-        }
+            public List<Teacher> GetTeachers()
+            {
+                throw new NotImplementedException();
+            }
 
-        public List<Student> GetStudents()
-        {
-            return Students;
-        }
+            public void AddStudent(Student student)
+            {
+                throw new NotImplementedException();
+            }
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+            public List<Student> GetStudents()
+            {
+                throw new NotImplementedException();
+            }
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello, World!");
+            }
         }
     }
 }
