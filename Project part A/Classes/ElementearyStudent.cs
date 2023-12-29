f62c71f127654b6fd4bd046331f277484ffccf3a
Yarik Zaychenko <yarikyarik20017@gmail.com>
2023-12-29 20:59:49 +0200

    cтворення класу для учнів молодшої школи, який унаслідує клас учнів

---
+++ Project part A/Classes/ElementearyStudent.cs
@@ -0,0 +1,31 @@
+﻿namespace Project_part_A.Classes
+{
+    public class ElementaryStudent : Student
+    {
+        public override int Age
+        {
+            get => base.Age; set
+            {
+                if (value > 10)
+                {
+                    throw new Exception("Надто дорослий для початкової школи");
+                }
+                base.Age = value;
+            }
+        }
+        public ElementaryStudent(string firstName, string lastName, int age) : base(firstName, lastName, age)
+        {
+
+        }
+
+        public override void ChooseSubject(Subject subject)
+        {
+            if (ChosenSubjects.Count > 4)
+            {
+                throw new Exception("Надто багато вибіркових дисциплін для початкової школи");
+            }
+            ChosenSubjects.Add(subject);
+        }
+
+    }
+}
